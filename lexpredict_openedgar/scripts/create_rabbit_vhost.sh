11c32d6735ec5191f09e4aad1c717a237fc04a46
Rick Albright <rla3rd@gmail.com>
2024-03-01 09:10:59 -0500

    rm this from cache

--- lexpredict_openedgar/scripts/create_rabbit_vhost.sh
+++
@@ -1,4 +0,0 @@
-rabbitmqctl add_user openedgar openedgar
-rabbitmqctl add_vhost openedgar
-rabbitmqctl set_permissions -p openedgar openedgar ".*" ".*" ".*"
-rabbitmqctl set_user_tags openedgar administrator
--- lexpredict_openedgar/scripts/run_celery.sh
+++
@@ -1,3 +0,0 @@
-source ../env/bin/activate
-source .env
-celery -A lexpredict_openedgar.taskapp worker --loglevel=ERROR -f celery.log -c16
